c8c045e9006eba57b3c3e71f577ba6d6d99f11a2
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-08-31 18:31:05 +0900

    009 prac fin

--- 009-app/src/index.js
+++ 009-app/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
-import App from "./App_1";
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
