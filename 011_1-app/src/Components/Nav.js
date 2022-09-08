b5e4e238a44dad5dc1d51175ccd00edaf71ab319
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-09-08 21:44:48 +0900

    file redirected

---
+++ 011_1-app/src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <ul>
+      <li>React</li>
+      <li>문서</li>
+      <li>자습서</li>
+      <li>블로그</li>
+      <li>커뮤니티</li>
+      <li>
+        <input type="text" placeholder="검색" />
+      </li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+}
