b894894b286b4e7b6d5995b2b83dadaa15c2b45e
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-09-08 22:45:35 +0900

    Nav css

--- 011_1-app/src/App.jsx
+++ 011_1-app/src/App.jsx
@@ -1,8 +1,43 @@
+import { createGlobalStyle } from "styled-components";
 import Nav from "./Components/Nav";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+li {
+  list-style: none;
+  color: red;
+  margin:0 50px;
+  text-align: center;
+  line-height: 30px;
+}
+
+h2{
+  color: #2e6fdf;
+  font-size: 30px;
+  margin-left: 5%;
+  margin-right: 15%;
+}
+
+nav{
+  display: flex;
+  margin-top:50px
+}
+
+ul{
+  display: flex;
+  
+}
+
+*{
+  background-color: black;
+}
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Nav />
     </>
   );
--- 011_1-app/src/Components/Nav.js
+++ 011_1-app/src/Components/Nav.js
@@ -2,17 +2,19 @@ import React from "react";
 
 export default function Nav() {
   return (
-    <ul>
-      <li>React</li>
-      <li>문서</li>
-      <li>자습서</li>
-      <li>블로그</li>
-      <li>커뮤니티</li>
-      <li>
-        <input type="text" placeholder="검색" />
-      </li>
-      <li></li>
-      <li></li>
-    </ul>
+    <nav>
+      <h2>React</h2>
+      <ul>
+        <li>문서</li>
+        <li>자습서</li>
+        <li>블로그</li>
+        <li>커뮤니티</li>
+        <li>
+          <input type="text" placeholder="검색" />
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
   );
 }
