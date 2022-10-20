f4980d7e32fecce51ee51ce8efebedd9a957cfac
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-10-20 16:14:31 +0900

    hook-custom hook fin

---
+++ 020-app/src/App.js
@@ -0,0 +1,13 @@
+import InputComponent from "./Components/InputComponents";
+import SomethingComponent from "./Components/SomethingInputComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <InputComponent />
+      <SomethingComponent />
+    </div>
+  );
+}
+
+export default App;
