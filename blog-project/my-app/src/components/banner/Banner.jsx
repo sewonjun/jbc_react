bf746cbbb1e2cc092da3abd8be28cd79f26d6c6b
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-12-19 19:58:55 +0900

    banner added

--- blog-project/my-app/src/components/banner/Banner.jsx
+++ blog-project/my-app/src/components/banner/Banner.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-export default function banner() {
-  return <div>banner</div>;
+export default function Banner() {
+  const [blogInfo, setBlogInfo] = useState(undefined);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/blog")
+      .then((blog) => {
+        setBlogInfo(blog.data);
+      })
+      .catch((e) => {
+        setBlogInfo(undefined);
+      });
+  }, []);
+  console.log(blogInfo);
+  return (
+    <div>
+      {blogInfo === undefined ? (
+        <div>로딩중..</div>
+      ) : (
+        <div>
+          <div className="banner">
+            <div className="max-width">
+              <div className="banner-contents">
+                <p className="sub-text">{blogInfo.subTitle}</p>
+                <p className="main-text">{blogInfo.mainTitle}</p>
+                <p className="description">{blogInfo.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
